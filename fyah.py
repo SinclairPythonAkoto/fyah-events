234336201645b4436249ae9bc69892e7bf7ddb28
SinclairPythonAkoto <sinclair.python@gmail.com>
2020-02-08 16:29:25 +0000

    email functionality added

--- fyah.py
+++ fyah.py
@@ -34,9 +34,35 @@ def login_required(f):
     return wrap
 
 
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def home():
-    return render_template('home.html')
+    if request.method == 'GET':
+        return render_template('home.html')
+    else:
+        name = request.form.get("name")
+        email = request.form.get("email")
+        # this will be the email address that the message will be sent to
+        myEmail = 'design.build.apply@gmail.com'
+        confirm = request.form.get("confirm")
+        sub = request.form.get("subject")
+        content = request.form.get("message_content")
+        if '@' not in email:
+            error = 'Your message did not send. Please provide a valid email'
+            return render_template('home.html', error=error)
+        elif '@' not in email and '@' not in confirm:
+            error = 'Please provide a valid email'
+            return render_template('home.html', error=error)
+        else:
+            msg = Message(f'{sub}', recipients=[myEmail])
+            msg.html = f"<p>From: {name}<br>Email: {email}<br><br>Subject:{sub}<br><br>{content}<br><br>To send a response, please go click <b><a href='https://www.bondrobotics.tech'>here</a></b>.</p>"
+
+            with app.open_resource('fyah_events_logo.jpg') as logo:
+                msg.attach('fyah_events_logo.jpg', 'image/jpeg', logo.read())
+
+            mail.send(msg)
+
+            flash(f'Thank you {name} for your message, Fyah Events will respond to you soon!')
+            return redirect(url_for('home'))
 
 @app.route('/login')
 def login():
