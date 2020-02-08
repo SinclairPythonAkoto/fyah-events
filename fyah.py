65d272368a5275d30d2b5f650a4690cfacc8d382
SinclairPythonAkoto <sinclair.python@gmail.com>
2020-02-08 15:29:51 +0000

    flask mail config added; login wrap added

--- fyah.py
+++ fyah.py
@@ -4,6 +4,36 @@ from flask_mail import Mail, Message
 
 app = Flask(__name__)
 
+# Flask-Mail config
+app.config.update(dict(
+    DEBUG = True,
+    MAIL_SERVER = 'smtp.gmail.com',
+    MAIL_PORT = 587,
+    MAIL_USE_TLS = True,
+    MAIL_USE_SSL = False,
+    MAIL_USERNAME = 'sinclairpythonakoto.mail@gmail.com',
+    MAIL_PASSWORD = 'Python2020',
+    MAIL_DEFAULT_SENDER = ('Fyah Events ©', 'sinclairpythonakoto.mail@gmail.com'), #('NAME OR TITLE OF SENDER', 'SENDER EMAIL ADDRESS')
+    MAIL_MAX_EMAILS = 25
+))
+
+mail = Mail(app)
+
+# set session secret key
+app.secret_key = "Sinclair"
+
+# redirect to login
+def login_required(f):
+    @wraps(f)
+    def wrap(*args, **kwargs):
+        if 'logged_in' in session:
+            return f(*args, **kwargs)
+        else:
+            flash('You need to sign in first')
+            return redirect(url_for('login'))
+    return wrap
+
+
 @app.route('/')
 def home():
     return render_template('home.html')
