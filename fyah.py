1259afed6d71819991ea61e55243090ed0c1a029
SinclairPythonAkoto <sinclair.python@gmail.com>
2020-02-14 12:37:49 +0000

    remove environment variables

--- fyah.py
+++ fyah.py
@@ -1,4 +1,4 @@
-import os
+# import os
 
 from flask import Flask, render_template, url_for, request, redirect, flash, session
 from functools import wraps
@@ -13,16 +13,16 @@ app.config.update(dict(
     MAIL_PORT = 587,
     MAIL_USE_TLS = True,
     MAIL_USE_SSL = False,
-    MAIL_USERNAME = os.getenv('GMAIL_EMAIL'),
-    MAIL_PASSWORD = os.getenv('GMAIL_PW'),
-    MAIL_DEFAULT_SENDER = ('Fyah Events ©', os.getenv('GMAIL_EMAIL')), #('NAME OR TITLE OF SENDER', 'SENDER EMAIL ADDRESS')
+    MAIL_USERNAME = os.getenv(),#'sinclair.python@gmail.com',
+    MAIL_PASSWORD = os.getenv(),#'Python2020',
+    MAIL_DEFAULT_SENDER = ('Fyah Events ©', os.getenv()),#'sinclair.python@gmail.com'), #('NAME OR TITLE OF SENDER', 'SENDER EMAIL ADDRESS')
     MAIL_MAX_EMAILS = 25
 ))
 
 mail = Mail(app)
 
 # set session secret key
-app.secret_key = "Sinclair"
+app.secret_key = os.getenv()#"Sinclair"
 
 # redirect to login
 def login_required(f):
@@ -49,8 +49,8 @@ def home():
         sub = request.form.get("subject")
         content = request.form.get("message_content")
 
-        # this will be the email address that the message will be sent to
-        myEmail = os.getenv('GMAIL_EMAIL')
+        # this will be the email address that the messages will be sent to
+        myEmail = os.getenv()#'fyahevents@gmail.com'
 
         if '@' not in email:
             error = 'Your message did not send. Please provide a valid email'
@@ -62,17 +62,17 @@ def home():
             if '@' not in email and '@' not in confirm:
                 error = 'Please provide a valid email'
                 return render_template('home.html', error=error)
-            else:
-                msg = Message(f'{sub}', recipients=[myEmail, os.getenv('INFO_MAIL')])
-                msg.html = f"<p>From: {name}<br>Email: {email}<br>Contact Number: {number}<br>Mobile Number: {mobile}<br><br>{content}<br><br>To send a response, please click <b><a href='https://fyahevents.onrender.com/admin'>here</a></b>.</p>"
+        else:
+            msg = Message(f'{sub}', recipients=[myEmail])
+            msg.html = f"<p>From: {name}<br>Email: {email}<br>Contact Number: {number}<br>Mobile Number: {mobile}<br><br>{content}<br><br>To send a response, please click <b><a href='https://fyahevents.onrender.com/admin'>here</a></b>.</p>"
 
-                with app.open_resource('fyah_events_logo.jpg') as logo:
-                    msg.attach('fyah_events_logo.jpg', 'image/jpeg', logo.read())
+            with app.open_resource('fyah_events_logo.jpg') as logo:
+                msg.attach('fyah_events_logo.jpg', 'image/jpeg', logo.read())
 
-                mail.send(msg)
+            mail.send(msg)
 
-                flash(f'Thank you {name} for your message, Fyah Events © will respond to you soon!')
-                return redirect(url_for('home'))
+            flash(f'Thank you {name} for your message, Fyah Events © will respond to you soon!')
+            return redirect(url_for('home'))
 
 @app.route('/sendSMS', methods=['GET', 'POST'])
 def sendSMS():
@@ -84,10 +84,10 @@ def sendSMS():
         txt = request.form.get('txt_content')
 
         from clockwork import clockwork
-        api = clockwork.API(os.getenv('SMS_API'),)	# this has been left blank to protect API identity
+        api = clockwork.API('9347aab600cbf889dac37eafbbff00c708a65e52',)	# this has been left blank to protect API identity
 
         message = clockwork.SMS(
-		    to = os.getenv('SMS_MOBILE'),
+		    to = '447711906690',
             message = f'From: {name}\nNumber: {number}\n\n{txt}\n\nFyah Events',
 		    from_name='Fyah Events') # from_name can max 11 characters long.
 
@@ -108,7 +108,8 @@ def login():
     if request.method == 'GET':
         return render_template('login.html')
     else:
-        if admin == os.getenv('ADMIN') and admin_pw == os.getenv('GMAIL_PW'):
+        if admin == 'admin' and admin_pw == '1234':
+        # if admin == os.getenv('ADMIN') and admin_pw == os.getenv('GMAIL_PW'):
             session['logged_in'] = True
             flash(f'Welcome back Fyah Events ©!')
             return redirect(url_for('admin'))
@@ -143,9 +144,7 @@ def email():
             return render_template('email.html', error=error)
         else:
             msg = Message(f'{sub}', recipients=[email.lower()])
-            # msg.body = f'Hi {person}!\n\nThank you for showing interest in Fyah Events ©.\n\n{content}\n\nKind regards\n\nFyah Events ©'
-
-            msg.html = f"<p>Hi {person}!</p><br><p>Thank you for showing interest in Fyah Events ©.</p><br><p>{content}</p><br><p>Kind regards</p><br><p><a href='https://fyahevents.onrender.com/'>Fyah Events ©</a></b></p>"
+            msg.html = f"<p>Hi {person}!</p><br><p>Thank you for showing interest in Fyah Events©.</p><br><p>{content}</p><br><p>Kind regards</p><br><p><a href='https://fyahevents.onrender.com/'>Fyah Events ©</a></b></p>"
 
             with app.open_resource('fyah_events_logo.jpg') as logo:
                 msg.attach('fyah_events_logo.jpg', 'image/jpeg', logo.read())
@@ -169,7 +168,7 @@ def sms():
         new_num = "".join(list_num)
 
         from clockwork import clockwork
-        api = clockwork.API(os.getenv('SMS_API'),)	# this has been left blank to protect API identity
+        api = clockwork.API(os.getenv(),)#'9347aab600cbf889dac37eafbbff00c708a65e52',)	# this has been left blank to protect API identity
 
         message = clockwork.SMS(
 		    to = f'{new_num}',
